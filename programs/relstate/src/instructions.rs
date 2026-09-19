19c2db3627b8b83b90ed6fe91e4f670e1949aeea
usereri <eryk.jamroz@gmail.com>
2026-09-19 17:13:51 +0200

    updated

--- programs/relstate/src/instructions.rs
+++ programs/relstate/src/instructions.rs
@@ -1,5 +1,9 @@
-pub mod initialize;
-pub mod increment;
+pub mod create_lease;
+pub mod fund_deposit;
+pub mod pay_rent;
+pub mod release_deposit;
 
-pub use initialize::*;
-pub use increment::*;
+pub use create_lease::*;
+pub use fund_deposit::*;
+pub use pay_rent::*;
+pub use release_deposit::*;
